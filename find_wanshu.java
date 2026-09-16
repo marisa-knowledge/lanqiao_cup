2219b27ee886ab012f1b831fc4db20b5f214326b
marisa-knowledge <479368398@qq.com>
2026-09-16 16:52:21 +0800

    Add files via upload
    for循环暴力嵌套

---
+++ find_wanshu.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class find_wanshu {
+    static void main() {
+        for (int i = 1; i <= 1000; i++) {
+            int sum = 0;
+            for(int j = 1; j < i ; j++){
+                if(i % j == 0){
+                    sum += j;
+                }
+            }
+            if(sum == i){
+                System.out.println(i);
+            }
+        }
+    }
+}
